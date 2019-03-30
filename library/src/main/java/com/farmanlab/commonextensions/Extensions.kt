c27fb82e2d81780ede704963e859311a0b8fefc9
farmanlab <farmlanlabdev@gmail.com>
2019-03-30 22:10:19 +0900

    [Update] update showToast Util.

--- library/src/main/java/com/farmanlab/commonextensions/Extensions.kt
+++ library/src/main/java/com/farmanlab/commonextensions/Extensions.kt
@@ -1,11 +1,34 @@
 package com.farmanlab.commonextensions
 
+import android.app.Activity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.annotation.LayoutRes
+import androidx.fragment.app.Fragment
 
 // region view
 fun ViewGroup.inflate(@LayoutRes layout: Int, attachToRoot: Boolean = false): View =
     LayoutInflater.from(context).inflate(layout, this, attachToRoot)
+// endregion
+
+// region activity
+fun Activity.toastShort(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+fun Activity.toastLong(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
+// endregion
+
+// region fragment
+fun Fragment.toastShort(message: String) {
+    activity?.let { Toast.makeText(activity, message, Toast.LENGTH_SHORT).show() }
+}
+
+fun Fragment.toastLong(message: String) {
+    activity?.let { Toast.makeText(activity, message, Toast.LENGTH_LONG).show() }
+}
 // endregion
