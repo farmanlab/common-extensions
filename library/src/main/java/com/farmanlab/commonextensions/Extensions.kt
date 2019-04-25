5add9828dd02d12009798154757c45b7b3cf0761
farmanlab <farmlanlabdev@gmail.com>
2019-04-25 10:07:31 +0900

    [Update] update color.

--- library/src/main/java/com/farmanlab/commonextensions/Extensions.kt
+++ library/src/main/java/com/farmanlab/commonextensions/Extensions.kt
@@ -7,11 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import androidx.annotation.ColorRes
 import androidx.annotation.LayoutRes
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import java.nio.charset.Charset
 
 // region common
+fun Context.loadColor(@ColorRes res: Int): Int = ContextCompat.getColor(this, res)
+
 fun Context.readFileFromAssets(fileName: String, charSet: Charset = Charset.defaultCharset()): String {
     val inputStream = assets.open(fileName)
     inputStream.bufferedReader(charSet).useLines { return it.map { row -> row }.joinToString("\n") }
